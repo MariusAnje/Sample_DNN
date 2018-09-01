4afaa8294e88b9c9cc2252dd5978e812cbc13419
Marius Anje <mariusanje@outlook.com>
2018-09-01 16:04:43 +0800

    good result in 8 bits

--- ImageNet/SampleNN.py
+++ ImageNet/SampleNN.py
@@ -2,7 +2,7 @@
 
 import torch
 
-class oSample(torch.autograd.Function):
+class xSample(torch.autograd.Function):
     def __init__(ctx):
         N = 16
         ctx.Q = pow(2, N-1) - 1
@@ -16,6 +16,20 @@ class oSample(torch.autograd.Function):
     def backward(ctx, g):
         return g
 
+class oSample(torch.autograd.Function):
+    def __init__(ctx):
+        N = 8
+        ctx.Q = pow(2, N-1) - 1
+        ctx.delt = pow(2,-7)
+    def forward(ctx, inputs):
+        Q = ctx.Q
+        delt = ctx.delt
+        M = (inputs/delt).round().to(torch.int16)
+        S = delt*M.to(torch.float32)
+        return S
+    def backward(ctx, g):
+        return g
+    
 def sampleStateDict(net):
     Dict = net.state_dict()
     Key = Dict.keys()
